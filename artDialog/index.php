db5d6e246053cc434f6313e99beed1bcd736557f
xululu <xuyanlu0424@163.com>
2015-10-08 21:07:47 +0800

    index.php modify

---
+++ artDialog/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="zh">
+	<head>
+		<meta charset="UTF-8">
+		<title>test</title>
+	</head>
+	<body>
+
+		<div>
+			<a href="./demo1.html">Demo1</a>	
+			<a href="./demo2.html">Demo2</a>	
+		<div>
+
+	</body>
+	<script src="./libs/jquery-1.10.2.js"></script>
+</html>
+
+
